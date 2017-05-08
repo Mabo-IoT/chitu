2f02b5190b54675c6542d57ee19219c82bb5d416
heathkang <3478325757@qq.com>
2017-05-08 14:23:00 +0800

    chitu works well 1.get data from redis 2.unpack data 3.send data to influxdb ,if can't connect to influxdb return data to redis

--- lib/Send.py
+++ lib/Send.py
@@ -1,11 +1,11 @@
 # --*-- coding:utf-8 --*--
 
 
-import msgpack
 import time
+
+import msgpack
 from logbook import Logger
 from ziyan.utils.database_wrapper import RedisWrapper, InfluxdbWrapper
-
 
 log = Logger('Send')
 
@@ -18,13 +18,22 @@ class Send:
     def __unpack(self):
         data_len = self.redis.get_len('data_queue')
         if data_len > 0:
+            # get data from redis
             self.data_original = self.redis.dequeue('data_queue')
+
+            # unpack data
             data = msgpack.unpackb(self.data_original)
-            measurement = msgpack.unpackb(data['measurement'])
-            tags = msgpack.unpackb(data['tag'])
-            fields = msgpack.unpackb(data['fields'])
+            data = self.msg_unpack(data)
+            data = self.byte_unpack(data)
+
+            # get influxdb send data
+            measurement = data['measurement']
+            tags = data['tags']
+            fields = data['fields']
             timestamp = data['time']
-            unit = msgpack.unpackb(data['unit'])
+            unit = data['unit']
+
+            # influxdb data structure
             josn_data = [
                 {
                     'measurement': measurement,
@@ -38,6 +47,49 @@ class Send:
             log.info('redis have no data')
             return None, None
 
+    def msg_unpack(self, bytes_dict):
+        """
+        lua to python3, lua's table will be transefer to python dict, but the key
+        and the value of dict is byte string, and bytes string can't be directly
+        used in send function from influxdb package.
+        :param bytes_dict: a dict whcih key and value is byte string.
+        :return: a user-friendly normal dict.
+        """
+        a = {}
+        if not isinstance(bytes_dict, dict):
+            return bytes_dict
+        for key, value in bytes_dict.items():
+            value = self.msg_unpack(value)
+            if isinstance(key, bytes):
+                key = key.decode()
+            if isinstance(value, bytes):
+                try:
+                    value = value.decode()
+                except:
+                    value = msgpack.unpackb(value)
+            a[key] = value
+
+        return a
+
+    def byte_unpack(self, bytes_dict):
+        """
+        lua to python3, lua's table will be transefer to python dict, but the key
+        and the value of dict is byte string, and bytes string can't be directly
+        used in send function from influxdb package.
+        :param bytes_dict: a dict whcih key and value is byte string.
+        :return: a user-friendly normal dict.
+        """
+        a = {}
+        if not isinstance(bytes_dict, dict):
+            return bytes_dict
+        for key, value in bytes_dict.items():
+            value = self.byte_unpack(value)
+            if isinstance(key, bytes):
+                key = key.decode()
+            if isinstance(value, bytes):
+                value = value.decode()
+            a[key] = value
+        return a
 
     def send(self):
         """
@@ -47,7 +99,8 @@ class Send:
         """
         data_handle, time_precision  = self.__unpack()
         if data_handle:
-            self.influxdb.send(data_handle, time_precision)
+            info = self.influxdb.send(data_handle, time_precision)
+            log.info('send data to inflxudb.{}, {}'.format(data_handle[0]['measurement'], info))
 
     def reque_data(self):
         """
@@ -67,7 +120,10 @@ class Send:
         while True:
             try:
                 self.send()
+
             except Exception as e:
                 log.error(e)
+
+                #can't connect to influxdb then repush data to redis
                 self.reque_data()
                 time.sleep(3)
