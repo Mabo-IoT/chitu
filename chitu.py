b57e4c35f90c70df54ed3151fc211d6d197b1fdf
piaoliu <441594700@qq.com>
2017-05-08 11:58:02 +0800

    多线程启动机制
    1.send 解包逻辑
    2.多线程启动，对多个 redis db 进行取数

--- chitu.py
+++ chitu.py
@@ -4,17 +4,66 @@ import os
 import time
 
 from logbook import Logger
+import threading
 
 from ziyan.utils.util import get_conf
 from ziyan.utils.logbook_wrapper import setup_logger
 from lib.Send import Send
 
-
 log = Logger('start')
+
 
 def start():
     conf = get_conf('conf/conf.toml')
     setup_logger(conf['log_configuration'])
+    workers = []
+    thread_set = dict()
+    for redis_address in conf['redis']['address']:
+        work = Send(conf, redis_address)
+        work.name = 'redis_' + str(redis_address['db'])
+        thread = threading.Thread(target=work.run, args=(), kwargs={},
+                                  name='%s' % work.name)
+        thread.setDaemon(True)
+        thread.start()
+        workers.append(work)
+        thread_set[work.name] = thread
+
+    watch = threading.Thread(target=watchdog, name='watchdog', args=(thread_set, workers))
+
+
+def watchdog(*args):
+    """
+    守护线程
+    :param args: 
+    :return: None
+    """
+    threads_name = {thread.name for thread in args[0].values()}
+    while True:
+
+        threads = set()
+
+        for item in threading.enumerate():
+            threads.add(item.name)
+
+        log.debug('\n' + str(threads) + '\n')
+
+        if threads - {'watchdog', 'MainThread'} != threads_name:
+            dead_threads = threads_name - (threads - {'watchdog', 'MainThread'})
+
+            # 获取死去线程的实例集
+            dead_threads = [thread for thread in args[1] if thread.name in dead_threads]
+
+            threads_set = dict()
+
+            for thread in dead_threads:
+                worker = threading.Thread(target=thread.work, args=(),
+                                          kwargs={},
+                                          name='%s' % thread.name)
+                worker.setDaemon(True)
+                worker.start()
+                threads_set[thread.name] = worker
+
+        time.sleep(10)
 
 
 if __name__ == '__main__':
--- lib/Send.py
+++ lib/Send.py
@@ -1,15 +1,41 @@
 # --*-- coding:utf-8 --*--
 
+import msgpack
+from logbook import Logger
 from ziyan.utils.database_wrapper import RedisWrapper, InfluxdbWrapper
 
 
+log = Logger('Send')
+
 class Send:
-    def __init__(self, conf):
-        pass
+    def __init__(self, conf, redis_address):
+        self.redis = RedisWrapper(redis_address)
 
     def unpack(self):
-        data=None
-        return data
+        data_len = self.redis.get_len('data_queue')
+        if data_len > 0:
+            self.rdata = self.redis.dequeue('data_queue')
+            data = msgpack.unpackb(self.rdata)
+            measurement = msgpack.unpackb(data['measurement'])
+            tags = msgpack.unpackb(data['tag'])
+            fields = msgpack.unpackb(data['fields'])
+            timestamp = data['time']
+            unit = msgpack.unpackb(data['unit'])
+            josn_data = [
+                {
+                    'measurement': measurement,
+                    'tags': tags,
+                    'time': timestamp,
+                    'fields': fields
+                 }
+            ]
+            return josn_data, unit
+        else:
+            log.info('redis have no data')
+            return None, None
 
     def send(self):
+        pass
+
+    def run(self):
         pass
